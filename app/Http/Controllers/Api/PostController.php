25da67cd42196107e21ea682c9967113cdb23968
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-31 11:55:49 -0500

    Creating our Resource PostCollection

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Post as PostRequest;
 use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -24,7 +25,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return 'Hello';
+        
     }
 
     /**
