44c7f93967c18ff68efc1e9cbb01b61c48bccbb5
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-31 13:20:01 -0500

    Modify layout & views UI app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                
+               <h1>Welcome!</h1> 
             </div>
         </div>
     </body>
