88284a5cbd2d9705b28e63b0efa57cf3994ef646
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-30 21:26:25 -0500

    Modify and declaring API Resources Routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::apiResource('posts', PostController::class);
