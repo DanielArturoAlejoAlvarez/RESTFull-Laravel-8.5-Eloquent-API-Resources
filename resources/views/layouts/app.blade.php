9323832ef666be46ecb0bc550a4a0be038bb7360
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-31 13:28:27 -0500

    Adding to view welcome our component VUE example-component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>API</title>
+    <title>API Resources</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
