0a190c2d0edb969a35b672c5de0a1fa47f4a0f7d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-31 13:14:52 -0500

    Adding from composer Laravel UI controllers,views,resources and auth

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::group(['prefix' => 'api'], function () {
     Route::apiResource('posts', PostController::class);
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
