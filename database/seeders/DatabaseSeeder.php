bdd54107a1cdc6e0c16c448262b3fec59a625fd1
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-31 21:44:17 -0500

    Modify DatabaseSeeder test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+            'name'      =>  'Mark Zuckerberg',
+            'email'     =>  'markzuck@yahoo.com',
+            'password'  =>  bcrypt('password1')
+        ]);
+
         \App\Models\Post::factory(18)->create();
     }
 }
